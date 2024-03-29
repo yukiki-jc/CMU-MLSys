finish hw2 but the precision is wrong

hw2/apps/mlp_resnet.py
@@ -13,7 +13,22 @@ np.random.seed(0)
 
 def ResidualBlock(dim, hidden_dim, norm=nn.BatchNorm1d, drop_prob=0.1):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    pre_residual_model = nn.Sequential(
+        nn.Linear(dim, hidden_dim), 
+        norm(hidden_dim),
+        nn.ReLU(),
+        nn.Dropout(drop_prob), # be carefule the drop out !!
+        nn.Linear(hidden_dim, dim), 
+        norm(dim)
+        )
+    model = nn.Sequential(
+        nn.Residual(pre_residual_model),
+        nn.ReLU()
+        )
+    return model 
+    
+    
     ### END YOUR SOLUTION
 
 
@@ -26,14 +41,49 @@ def MLPResNet(
     drop_prob=0.1,
 ):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    pre_model = nn.Sequential(
+        nn.Linear(dim, hidden_dim),
+        nn.ReLU()
+    )
+    residual_blocks = nn.Sequential(
+        *[ResidualBlock(hidden_dim, hidden_dim // 2, norm, drop_prob) for _ in range(num_blocks)]
+    )
+    post_model = nn.Linear(hidden_dim, num_classes)
+    return nn.Sequential(
+        pre_model, 
+        residual_blocks,
+        post_model
+    )
+   
     ### END YOUR SOLUTION
 
 
-def epoch(dataloader, model, opt=None):
+
+def epoch(dataloader, model, opt : ndl.optim.Optimizer = None):
     np.random.seed(4)
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    loss_func = nn.SoftmaxLoss()
+    correct, loss_sum, n_step, n_samples= 0., 0., 0, 0
+    if opt:
+        model.train()
+    else:
+        model.eval()
+    for X, y in dataloader:
+        if opt:
+            opt.reset_grad()
+        pred = model(X)
+        loss = loss_func(pred, y)
+        correct += (pred.numpy().argmax(axis=1) == y.numpy()).sum()
+        if opt:
+            loss.backward()
+            opt.step()
+        loss_sum += loss.numpy()
+        n_step += 1
+        n_samples += X.shape[0]
+    
+    # NOTE (1 - mean of acc over iterations) is not accurate enough
+    return (1 - correct / n_samples), loss_sum / n_step
     ### END YOUR SOLUTION
 
 
@@ -47,8 +97,19 @@ def train_mnist(
     data_dir="data",
 ):
     np.random.seed(4)
+    import os 
+    train_data = ndl.data.MNISTDataset(os.path.join(data_dir, "train-images-idx3-ubyte.gz"), os.path.join(data_dir, "train-labels-idx1-ubyte.gz"))
+    test_data = ndl.data.MNISTDataset(os.path.join(data_dir, "t10k-images-idx3-ubyte.gz"), os.path.join(data_dir, "t10k-labels-idx1-ubyte.gz"))
+    train_dataloader = ndl.data.DataLoader(train_data, batch_size)
+    test_dataloader = ndl.data.DataLoader(test_data, batch_size)
+    model = MLPResNet(28 * 28, hidden_dim)
+    optim = optimizer(model.parameters(), lr, weight_decay=weight_decay)
+    for i in range(epochs):
+        train_acc, train_loss = epoch(train_dataloader, model, optim)
+        test_acc, test_loss = epoch(test_dataloader, model)
+    return train_acc, train_loss, test_acc, test_loss
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    # raise NotImplementedError()
     ### END YOUR SOLUTION
 
 

hw2/python/needle/data/data_basic.py
@@ -50,22 +50,34 @@ class DataLoader:
         batch_size: Optional[int] = 1,
         shuffle: bool = False,
     ):
-
         self.dataset = dataset
         self.shuffle = shuffle
         self.batch_size = batch_size
         if not self.shuffle:
             self.ordering = np.array_split(np.arange(len(dataset)), 
                                            range(batch_size, len(dataset), batch_size))
+        else: 
+            order = np.arange(len(self.dataset))
+            np.random.shuffle(order)
+            self.ordering = np.array_split(order, range(self.batch_size, len(self.dataset), self.batch_size))
 
     def __iter__(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        print("initializing iter object")
+        self.start = 0
         ### END YOUR SOLUTION
         return self
 
     def __next__(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        if self.start == len(self.ordering):
+            raise StopIteration
+        r = self.dataset[self.ordering[self.start]]
+        
+        r = [Tensor(x, dtype=x.dtype) for x in r]
+        self.start += 1
+        return r
         ### END YOUR SOLUTION
 

hw2/python/needle/data/data_transforms.py
@@ -20,7 +20,10 @@ class RandomFlipHorizontal(Transform):
         """
         flip_img = np.random.rand() < self.p
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        if flip_img: 
+            img = np.fliplr(img)
+        return img 
         ### END YOUR SOLUTION
 
 
@@ -36,7 +39,23 @@ class RandomCrop(Transform):
             H x W x C NAArray of cliped image
         Note: generate the image shifted by shift_x, shift_y specified below
         """
+
         shift_x, shift_y = np.random.randint(low=-self.padding, high=self.padding+1, size=2)
+        print(shift_x, shift_y)
+        h, w = img.shape[0], img.shape[1]
+        if abs(shift_x) >= h or abs(shift_y) >= w: 
+            return np.zeros_like(img)
+        img = np.roll(img, -shift_x, axis=0)
+        img = np.roll(img, -shift_y, axis=1)
+        if shift_x > 0:
+            img[-shift_x:, :, :] = 0
+        else: 
+            img[:-shift_x, :, :] = 0
+        if shift_y > 0:
+            img[:, -shift_y:, :] = 0
+        else: 
+            img[:, :-shift_y, :] = 0
+        return img
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        # raise NotImplementedError()
         ### END YOUR SOLUTION

hw2/python/needle/data/datasets/mnist_dataset.py
@@ -1,7 +1,8 @@
 from typing import List, Optional
 from ..data_basic import Dataset
 import numpy as np
-
+import gzip
+import struct
 class MNISTDataset(Dataset):
     def __init__(
         self,
@@ -10,15 +11,51 @@ class MNISTDataset(Dataset):
         transforms: Optional[List] = None,
     ):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        self.transforms = transforms
+        images = []
+        def read_header(f):
+            int_bytes = 4
+            # read image
+            magic_num_buf = f.read(int_bytes)
+            magic_num = struct.unpack('>i', magic_num_buf)[0]
+            dim = magic_num & 0x000000ff
+            int_buf = f.read(int_bytes * dim)
+            pack_str = '>' + 'i' * dim
+            dims = struct.unpack(pack_str, int_buf)
+            return dims
+
+        with gzip.open(image_filename, 'rb') as f:
+            dims = read_header(f)
+            for i in range(dims[0]):
+                length = dims[1] * dims[2]
+                buf = f.read(length)
+                pack_str = 'B' * length
+                r = list(struct.unpack(pack_str, buf))
+                images.append(r)
+
+        with gzip.open(label_filename, 'rb') as f:
+            dims = read_header(f)
+            length = dims[0]
+            buf = f.read(length)
+            pack_str = 'B' * length
+            label = list(struct.unpack(pack_str, buf))
+        self.images, self.labels = np.array(images, dtype=np.float32) / 255, np.array(label, dtype=np.uint8)
         ### END YOUR SOLUTION
 
     def __getitem__(self, index) -> object:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        r = self.images[index]
+        if self.transforms is not None:
+            r = r.reshape((28, 28, -1))
+            r = self.apply_transforms(r)
+            r = r.reshape(-1, 28 * 28)
+        return r, self.labels[index]
         ### END YOUR SOLUTION
 
     def __len__(self) -> int:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        return self.images.shape[0]
         ### END YOUR SOLUTION